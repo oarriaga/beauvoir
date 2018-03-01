e351b78bedd3bf4833bc3083a4b4cb7f5fe5526e
Octavio Arriaga <arriaga.camargo@gmail.com>
2018-03-01 11:58:08 +0100

    Added new bounding boxes plotting functioj

--- src/plot_bounding_boxes.py
+++ src/plot_bounding_boxes.py
@@ -7,6 +7,26 @@ import cv2
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 
+def SaveFigureAsImage(fileName, fig=None, **kwargs):
+    fig_size = fig.get_size_inches()
+    w, h = fig_size[0], fig_size[1]
+    fig.patch.set_alpha(0)
+    if kwargs in 'orig_size':
+        w, h = kwargs['orig_size']
+        w2 = fig_size[0]
+        fig.set_size_inches([(w2/w)*w, (w2/w)*h])
+        fig.set_dpi((w2/w)*fig.get_dpi())
+    a = fig.gca()
+    a.set_frame_on(False)
+    a.set_xticks([])
+    a.set_yticks([])
+    plt.axis('off')
+    plt.xlim(0, h)
+    plt.ylim(w, 0)
+    fig.savefig(fileName, transparent=True, bbox_inches='tight',
+                pad_inches=0)
+
+
 def denormalize_box(box_coordinates, image_shape):
     h, w = image_shape
     x_min = int(box_coordinates[0] * w)
@@ -49,17 +69,27 @@ def plot_single_box_data(box_data, original_image_array, arg_to_class=None,
     cv2.putText(original_image_array, display_text, (x_min, y_min - 10),
                 font, .25, color, 1, cv2.LINE_AA)
     thickness = 1
-    cv2.rectangle(original_image_array, (x_min, y_min),
-                  (x_max, y_max), color, thickness)
+    # print('y_min', y_min)
+    # print('y_max', y_max)
+    cv2.rectangle(original_image_array, (x_min, y_max),
+                  (x_max, y_min), color, thickness)
 
 
+num_images = 100
 dataset_path = '../data/detection_data/'
 xml_parser = XMLParser(dataset_path)
 arg_to_class = xml_parser.arg_to_class
 data = xml_parser.load_data()
-path, box_data = list(data.items())[0]
-image_array = cv2.imread(path)
-colors = get_colors(55)
-plot_box_data(box_data, image_array, arg_to_class, colors=colors)
-plt.imshow(image_array)
-plt.show()
+for image_arg in range(num_images):
+    path, box_data = list(data.items())[image_arg]
+    image_array = cv2.imread(path)
+    image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+    colors = get_colors(55)
+    plot_box_data(box_data, image_array, arg_to_class, colors=colors)
+    plt.axis('off')
+    image_name = 'gt/ground_truths_' + str(image_arg)
+    plt.imshow(image_array)
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+    # plt.show()
+    plt.savefig(image_name, bbox_inches='tight')
+    # SaveFigureAsImage(image_name, plt.gca())
